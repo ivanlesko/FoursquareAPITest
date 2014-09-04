524302ffe4f28405c4a1552f6ef838a17d578545
ivanlesko <mail@ivanlesko.com>
2014-09-04 14:24:56 -0700

    Initial Commit.

---
+++ FoursquareTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FoursquareTest
+//
+//  Created by Ivan Lesko on 9/4/14.
+//  Copyright (c) 2014 GeneralUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
